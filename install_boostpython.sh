e007794996c6c335bc19ece72b337f74ae107c86
ShannonDing <libya_003@163.com>
2018-12-04 23:41:47 +0800

    Add Boost Python build shell

---
+++ install_boostpython.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+VERSION=1.58.0
+BOOST=boost_1_58_0
+
+if [ ! -d ${HOME}/${BOOST} ]; then
+    wget -O ${HOME}/${BOOST}.tar.gz http://sourceforge.net/projects/boost/files/boost/${VERSION}/${BOOST}.tar.gz
+    if [ $? -ne 0 ];then
+        exit 1
+    fi
+    tar -xzf ${HOME}/${BOOST}.tar.gz -C ${HOME}
+    if [ $? -ne 0 ];then
+        exit 1
+    fi
+fi
+
+cd ${HOME}/${BOOST}
+
+./bootstrap.sh --prefix=/usr/local --with-libraries=python
+if [ $? -ne 0 ];then
+    exit 1
+fi
+sudo   ./bjam cflags="-fPIC" cxxflags="-fPIC -Wno-unused-local-typedefs -Wno-strict-aliasing" link=static \
+       --with-python  \
+       -a install \
+       > ${HOME}/build.log
+if [ $? -ne 0 ];then
+    exit 1
+fi
+sudo   ./bjam cflags="-fPIC" cxxflags="-fPIC -Wno-unused-local-typedefs -Wno-strict-aliasing" link=shared \
+       --with-python  \
+       -a install \
+       > ${HOME}/build.log
+
+if [ $? != 0 ]; then
+    tail -n 300 ${HOME}/build.log
+fi
