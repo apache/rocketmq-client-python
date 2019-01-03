a9f4cfdfdf72c6e2868a5267d88d08464f6b353d
messense <messense@icloud.com>
2019-01-03 21:27:43 +0800

    Check ffi return code and raise exception when it errors (#8)

--- rocketmq/client.py
+++ rocketmq/client.py
@@ -6,6 +6,7 @@ from .ffi import (
     dll, _CSendResult, MSG_CALLBACK_FUNC, _CMessageQueue, _CPullStatus,
     _CConsumeStatus, MessageModel,
 )
+from .exceptions import ffi_check
 
 
 SendResult = namedtuple('SendResult', ['status', 'msg_id', 'offset'])
@@ -17,22 +18,22 @@ class Message(object):
 
     def __del__(self):
         if self._handle is not None:
-            dll.DestroyMessage(self._handle)
+            ffi_check(dll.DestroyMessage(self._handle))
 
     def set_keys(self, keys):
-        return dll.SetMessageKeys(self._handle, keys.encode('utf-8'))
+        ffi_check(dll.SetMessageKeys(self._handle, keys.encode('utf-8')))
 
     def set_tags(self, tags):
-        return dll.SetMessageTags(self._handle, tags.encode('utf-8'))
+        ffi_check(dll.SetMessageTags(self._handle, tags.encode('utf-8')))
 
     def set_body(self, body):
-        return dll.SetMessageBody(self._handle, body.encode('utf-8'))
+        ffi_check(dll.SetMessageBody(self._handle, body.encode('utf-8')))
 
     def set_property(self, key, value):
-        return dll.SetMessageProperty(self._handle, key.encode('utf-8'), value.encode('utf-8'))
+        ffi_check(dll.SetMessageProperty(self._handle, key.encode('utf-8'), value.encode('utf-8')))
 
     def set_delay_time_level(self, delay_time_level):
-        return dll.SetDelayTimeLevel(self._handle, delay_time_level)
+        ffi_check(dll.SetDelayTimeLevel(self._handle, delay_time_level))
 
     @property
     def _as_parameter_(self):
@@ -68,33 +69,38 @@ class Producer(object):
 
     def __del__(self):
         if self._handle is not None:
-            dll.DestroyProducer(self._handle)
+            ffi_check(dll.DestroyProducer(self._handle))
 
     def send_sync(self, msg):
         cres = _CSendResult()
-        dll.SendMessageSync(self._handle, msg, ctypes.pointer(cres))
+        ffi_check(dll.SendMessageSync(self._handle, msg, ctypes.pointer(cres)))
         return SendResult(cres.sendStatus, cres.msgId.decode('utf-8'), cres.offset)
 
     def send_oneway(self, msg):
-        return dll.SendMessageOneway(self._handle, msg)
+        ffi_check(dll.SendMessageOneway(self._handle, msg))
 
     def set_group(self, group_name):
-        return dll.SetProducerGroupName(group_name.encode('utf-8'))
+        ffi_check(dll.SetProducerGroupName(group_name.encode('utf-8')))
 
     def set_namesrv_addr(self, addr):
-        return dll.SetProducerNameServerAddress(self._handle, addr.encode('utf-8'))
+        ffi_check(dll.SetProducerNameServerAddress(self._handle, addr.encode('utf-8')))
 
     def set_namesrv_domain(self, domain):
-        return dll.SetProducerNameServerDomain(self._handle, domain.encode('utf-8'))
+        ffi_check(dll.SetProducerNameServerDomain(self._handle, domain.encode('utf-8')))
 
     def set_session_credentials(self, access_key, access_secret, channel):
-        return dll.SetProducerSessionCredentials(self._handle, access_key.encode('utf-8'), access_secret.encode('utf-8'), channel.encode('utf-8'))
+        ffi_check(dll.SetProducerSessionCredentials(
+            self._handle,
+            access_key.encode('utf-8'),
+            access_secret.encode('utf-8'),
+            channel.encode('utf-8')
+        ))
 
     def start(self):
-        return dll.StartProducer(self._handle)
+        ffi_check(dll.StartProducer(self._handle))
 
     def shutdown(self):
-        return dll.ShutdownProducer(self._handle)
+        ffi_check(dll.ShutdownProducer(self._handle))
 
 
 class PushConsumer(object):
@@ -106,28 +112,33 @@ class PushConsumer(object):
 
     def __del__(self):
         if self._handle is not None:
-            dll.DestroyPushConsumer(self._handle)
+            ffi_check(dll.DestroyPushConsumer(self._handle))
 
     def set_message_model(self, model):
-        dll.SetPushConsumerMessageModel(self._handle, model)
+        ffi_check(dll.SetPushConsumerMessageModel(self._handle, model))
 
     def start(self):
-        return dll.StartPushConsumer(self._handle)
+        ffi_check(dll.StartPushConsumer(self._handle))
 
     def shutdown(self):
-        return dll.ShutdownPushConsumer(self._handle)
+        ffi_check(dll.ShutdownPushConsumer(self._handle))
 
     def set_group(self, group_id):
-        return dll.SetPushConsumerGroupID(group_id.encode('utf-8'))
+        ffi_check(dll.SetPushConsumerGroupID(group_id.encode('utf-8')))
 
     def set_namesrv_addr(self, addr):
-        return dll.SetPushConsumerNameServerAddress(self._handle, addr.encode('utf-8'))
+        ffi_check(dll.SetPushConsumerNameServerAddress(self._handle, addr.encode('utf-8')))
 
     def set_namesrv_domain(self, domain):
-        return dll.SetPushConsumerNameServerDomain(self._handle, domain.encode('utf-8'))
+        ffi_check(dll.SetPushConsumerNameServerDomain(self._handle, domain.encode('utf-8')))
 
     def set_session_credentials(self, access_key, access_secret, channel):
-        return dll.SetPushConsumerSessionCredentials(self._handle, access_key.encode('utf-8'), access_secret.encode('utf-8'), channel.encode('utf-8'))
+        ffi_check(dll.SetPushConsumerSessionCredentials(
+            self._handle,
+            access_key.encode('utf-8'),
+            access_secret.encode('utf-8'),
+            channel.encode('utf-8')
+        ))
 
     def subscribe(self, topic, callback, expression='*'):
         def _on_message(consumer, msg):
@@ -143,7 +154,7 @@ class PushConsumer(object):
 
             return _CConsumeStatus.CONSUME_SUCCESS.value
 
-        dll.Subscribe(self._handle, topic.encode('utf-8'), expression.encode('utf-8'))
+        ffi_check(dll.Subscribe(self._handle, topic.encode('utf-8'), expression.encode('utf-8')))
         self._register_callback(_on_message)
 
     def _register_callback(self, callback):
@@ -154,21 +165,21 @@ class PushConsumer(object):
 
         func = MSG_CALLBACK_FUNC(callback)
         self._refs.append(func)
-        return register_func(self._handle, func)
+        ffi_check(register_func(self._handle, func))
 
     def _unregister_callback(self):
         if self._orderly:
-            return dll.UnregisterMessageCallbackOrderly(self._handle)
-        return dll.UnregisterMessageCallback(self._handle)
+            ffi_check(dll.UnregisterMessageCallbackOrderly(self._handle))
+        ffi_check(dll.UnregisterMessageCallback(self._handle))
 
     def set_thread_count(self, thread_count):
-        return dll.SetPushConsumerThreadCount(self._handle, thread_count)
+        ffi_check(dll.SetPushConsumerThreadCount(self._handle, thread_count))
 
     def set_message_batch_max_size(self, max_size):
-        return dll.SetPushConsumerMessageBatchMaxSize(self._handle, max_size)
+        ffi_check(dll.SetPushConsumerMessageBatchMaxSize(self._handle, max_size))
 
     def set_instance_name(self, name):
-        return dll.SetPushConsumerInstanceName(self._handle, name.encode('utf-8'))
+        ffi_check(dll.SetPushConsumerInstanceName(self._handle, name.encode('utf-8')))
 
 
 class PullConsumer(object):
@@ -177,30 +188,40 @@ class PullConsumer(object):
 
     def __del__(self):
         if self._handle is not None:
-            dll.DestroyPullConsumer(self._handle)
+            ffi_check(dll.DestroyPullConsumer(self._handle))
 
     def start(self):
-        return dll.StartPullConsumer(self._handle)
+        ffi_check(dll.StartPullConsumer(self._handle))
 
     def shutdown(self):
-        return dll.ShutdownPullConsumer(self._handle)
+        ffi_check(dll.ShutdownPullConsumer(self._handle))
 
     def set_group(self, group_id):
-        return dll.SetPullConsumerGroupID(group_id.encode('utf-8'))
+        ffi_check(dll.SetPullConsumerGroupID(group_id.encode('utf-8')))
 
     def set_namesrv_addr(self, addr):
-        return dll.SetPullConsumerNameServerAddress(self._handle, addr.encode('utf-8'))
+        ffi_check(dll.SetPullConsumerNameServerAddress(self._handle, addr.encode('utf-8')))
 
     def set_namesrv_domain(self, domain):
-        return dll.SetPullConsumerNameServerDomain(self._handle, domain.encode('utf-8'))
+        ffi_check(dll.SetPullConsumerNameServerDomain(self._handle, domain.encode('utf-8')))
 
     def set_session_credentials(self, access_key, access_secret, channel):
-        return dll.SetPullConsumerSessionCredentials(self._handle, access_key.encode('utf-8'), access_secret.encode('utf-8'), channel.encode('utf-8'))
+        ffi_check(dll.SetPullConsumerSessionCredentials(
+            self._handle,
+            access_key.encode('utf-8'),
+            access_secret.encode('utf-8'),
+            channel.encode('utf-8')
+        ))
 
     def pull(self, topic):
         message_queue = ctypes.POINTER(_CMessageQueue)()
         queue_size = ctypes.c_int()
-        dll.FetchSubscriptionMessageQueues(self._handle, topic.encode('utf-8'), ctypes.pointer(message_queue), ctypes.pointer(queue_size))
+        ffi_check(dll.FetchSubscriptionMessageQueues(
+            self._handle,
+            topic.encode('utf-8'),
+            ctypes.pointer(message_queue),
+            ctypes.pointer(queue_size)
+        ))
         for i in range(int(queue_size.value)):
             tmp_offset = ctypes.c_longlong()
             while True:
@@ -212,5 +233,5 @@ class PullConsumer(object):
                         yield RecvMessage(pull_res.msgFoundList[i])
                 elif pull_res.pullStatus == _CPullStatus.NO_MATCHED_MSG:
                     break
-                dll.ReleasePullResult(pull_res)
-        dll.ReleaseSubscriptionMessageQueue(message_queue)
+                ffi_check(dll.ReleasePullResult(pull_res))
+        ffi_check(dll.ReleaseSubscriptionMessageQueue(message_queue))
---
+++ rocketmq/exceptions.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+from .ffi import _CStatus
+
+
+_EXCEPTION_MAP = {}
+
+
+def _register(status_code):
+    def register(cls):
+        _EXCEPTION_MAP[status_code] = cls
+        return cls
+    return register
+
+
+def ffi_check(status_code):
+    if status_code == _CStatus.OK:
+        return
+    exc_cls = _EXCEPTION_MAP.get(status_code, RocketMQException)
+    raise exc_cls()
+
+
+class RocketMQException(Exception):
+    '''RocketMQ exception base class'''
+    pass
+
+
+@_register(_CStatus.NULL_POINTER)
+class NullPointerException(RocketMQException):
+    pass
+
+
+@_register(_CStatus.MALLOC_FAILED)
+class MallocFailed(RocketMQException):
+    pass
+
+
+class ProducerException(RocketMQException):
+    pass
+
+
+@_register(_CStatus.PRODUCER_START_FAILED)
+class ProducerStartFailed(ProducerException):
+    pass
+
+
+@_register(_CStatus.PRODUCER_SEND_SYNC_FAILED)
+class ProducerSendSyncFailed(ProducerException):
+    pass
+
+
+@_register(_CStatus.PRODUCER_SEND_ONEWAY_FAILED)
+class ProducerSendOnewayFailed(ProducerException):
+    pass
+
+
+@_register(_CStatus.PRODUCER_SEND_ORDERLY_FAILED)
+class ProducerSendOrderlyFailed(ProducerException):
+    pass
+
+
+class ConsumerException(RocketMQException):
+    pass
+
+
+@_register(_CStatus.PUSHCONSUMER_START_FAILED)
+class PushConsumerStartFailed(ConsumerException):
+    pass
+
+
+@_register(_CStatus.PULLCONSUMER_START_FAILED)
+class PullConsumerStartFailed(ConsumerException):
+    pass
+
+
+@_register(_CStatus.PULLCONSUMER_FETCH_MQ_FAILED)
+class PullConsumerFetchMQFailed(ConsumerException):
+    pass
+
+
+@_register(_CStatus.PULLCONSUMER_FETCH_MQ_FAILED)
+class PullConsumerFetchMessageFailed(ConsumerException):
+    pass
