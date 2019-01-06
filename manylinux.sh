911a629ae79187c0e3a3b7d19149455cd487c084
messense <messense@icloud.com>
2019-01-06 14:39:44 +0800

    Upgrade libevent and jsoncpp

--- manylinux.sh
+++ manylinux.sh
@@ -9,10 +9,10 @@ curl -sqL https://zlib.net/zlib-1.2.11.tar.gz | tar -xz -C /tmp
 cd /tmp/zlib-1.2.11/ && ./configure --prefix=/usr && make && make install && cd -
 
 # Build rocketmq-client-cpp
-git clone --depth=1 --branch=fix https://github.com/messense/rocketmq-client-cpp.git /tmp/rocketmq-client-cpp
+git clone --depth=1 --branch=release https://github.com/messense/rocketmq-client-cpp.git /tmp/rocketmq-client-cpp
 mkdir -p /tmp/rocketmq-client-cpp/tmp_down_dir
-curl -sqL -o /tmp/rocketmq-client-cpp/tmp_down_dir/libevent-release-2.0.22-stable.zip https://github.com/libevent/libevent/archive/release-2.0.22-stable.zip
-curl -sqL -o /tmp/rocketmq-client-cpp/tmp_down_dir/jsoncpp-0.10.6.zip https://github.com/open-source-parsers/jsoncpp/archive/0.10.6.zip
+curl -sqL -o /tmp/rocketmq-client-cpp/tmp_down_dir/libevent-release-2.1.8-stable.zip https://github.com/libevent/libevent/archive/release-2.1.8-stable.zip
+curl -sqL -o /tmp/rocketmq-client-cpp/tmp_down_dir/jsoncpp-0.10.7.zip https://github.com/open-source-parsers/jsoncpp/archive/0.10.7.zip
 curl -sqL -o /tmp/rocketmq-client-cpp/tmp_down_dir/boost_1_58_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.gz
 cd /tmp/rocketmq-client-cpp && bash build.sh && cd -
 cp /tmp/rocketmq-client-cpp/bin/librocketmq.so /io/rocketmq/librocketmq.so
