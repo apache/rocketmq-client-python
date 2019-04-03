44b6ad1c1d8114c1d807dff9db847eb6c0f791d1
messense <messense@icloud.com>
2019-04-03 10:55:38 +0800

    Bump version: 0.3.10 → 0.3.11

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ cmdclass['install'] = InstallPlatlib
 
 setup(
     name='rocketmq',
-    version='0.3.10',
+    version='0.3.11',
     author='messense',
     author_email='messense@icloud.com',
     packages=find_packages(exclude=('tests', 'tests.*')),
