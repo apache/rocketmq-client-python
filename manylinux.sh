28f71fa88fd447306fbf7f5b7688f565216d6142
messense <messense@icloud.com>
2019-07-24 13:22:53 +0800

    Update rocketmq-client-cpp to 2ed69b341cc750aaadfc24a8a872db5f78db2ab6

--- manylinux.sh
+++ manylinux.sh
@@ -14,7 +14,7 @@ mkdir -p /tmp/rocketmq-client-cpp/tmp_down_dir
 curl -sqL -o /tmp/rocketmq-client-cpp/tmp_down_dir/libevent-release-2.1.8-stable.zip https://github.com/libevent/libevent/archive/release-2.1.8-stable.zip
 curl -sqL -o /tmp/rocketmq-client-cpp/tmp_down_dir/jsoncpp-0.10.7.zip https://github.com/open-source-parsers/jsoncpp/archive/0.10.7.zip
 curl -sqL -o /tmp/rocketmq-client-cpp/tmp_down_dir/boost_1_58_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.gz
-cd /tmp/rocketmq-client-cpp && git checkout dd328fcd017e9d3e1c1b4cdc7cdef5bdee5b9405 && bash build.sh && cd -
+cd /tmp/rocketmq-client-cpp && git checkout 2ed69b341cc750aaadfc24a8a872db5f78db2ab6 && bash build.sh && cd -
 cp /tmp/rocketmq-client-cpp/bin/librocketmq.so /io/rocketmq/librocketmq.so
 
 # Build wheels
