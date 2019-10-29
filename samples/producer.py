d8bc677b123009b748fea4af8f9fce29f6c7ac8a
duhenglucky <duheng0522@gmail.com>
2019-10-29 10:50:21 +0800

    fix(sample)fix python3 support

--- samples/producer.py
+++ samples/producer.py
@@ -1,19 +1,21 @@
-# /*
-# * Licensed to the Apache Software Foundation (ASF) under one or more
-# * contributor license agreements.  See the NOTICE file distributed with
-# * this work for additional information regarding copyright ownership.
-# * The ASF licenses this file to You under the Apache License, Version 2.0
-# * (the "License"); you may not use this file except in compliance with
-# * the License.  You may obtain a copy of the License at
-# *
-# *     http://www.apache.org/licenses/LICENSE-2.0
-# *
-# * Unless required by applicable law or agreed to in writing, software
-# * distributed under the License is distributed on an "AS IS" BASIS,
-# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# * See the License for the specific language governing permissions and
-# * limitations under the License.
-# */
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 from rocketmq.client import Producer, Message, TransactionMQProducer, TransactionStatus
 
 import time
@@ -38,8 +40,8 @@ def send_message_sync(count):
     for n in range(count):
         msg = create_message()
         ret = producer.send_sync(msg)
-        print 'send message status: ' + str(ret.status) + ' msgId: ' + ret.msg_id + ' offset: ' + str(ret.offset)
-    print 'send sync message done'
+        print ('send message status: ' + str(ret.status) + ' msgId: ' + ret.msg_id + ' offset: ' + str(ret.offset))
+    print ('send sync message done')
     producer.shutdown()
 
 
@@ -50,18 +52,18 @@ def send_orderly_with_sharding_key(count):
     for n in range(count):
         msg = create_message()
         ret = producer.send_orderly_with_sharding_key(msg, 'orderId')
-        print 'send message status: ' + str(ret.status) + ' msgId: ' + ret.msg_id 
-    print 'send sync message done'
+        print ('send message status: ' + str(ret.status) + ' msgId: ' + ret.msg_id)
+    print ('send sync message done')
     producer.shutdown()
 
 
 def check_callback(msg):
-    print 'check: ' + msg.id.decode('utf-8')
+    print ('check: ' + msg.id.decode('utf-8'))
     return TransactionStatus.COMMIT
 
 
 def local_execute(msg, user_args):
-    print 'local   ' + msg.id.decode('utf-8')
+    print ('local   ' + msg.id.decode('utf-8'))
     return TransactionStatus.UNKNOWN
 
 
@@ -72,8 +74,8 @@ def send_transaction_message(count):
     for n in range(count):
         msg = create_message()
         ret = transactionMQProducer.send_message_in_transaction(msg, local_execute, None)
-        print 'send message status: ' + str(ret.status) + ' msgId: ' + ret.msg_id 
-    print 'send transaction message done'
+        print ('send message status: ' + str(ret.status) + ' msgId: ' + ret.msg_id)
+    print ('send transaction message done')
 
     while True:
         time.sleep(3600)
@@ -81,5 +83,5 @@ def send_transaction_message(count):
 
 
 if __name__ == '__main__':
-    send_transaction_message(10)
+    send_message_sync(10)
 
